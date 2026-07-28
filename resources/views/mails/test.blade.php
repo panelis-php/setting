06f0c922ff408180df77411bba2028c679992808
Yugo <dedy.yugo.purwanto@gmail.com>
2026-07-28 20:27:38 +0700

    Add test link in test mail (#20)

--- resources/views/mails/test.blade.php
+++ resources/views/mails/test.blade.php
@@ -1,3 +1,7 @@
 <x-mail::message>
-    @lang('setting::mail.test_content')
+  @lang('setting::mail.test_content')
+
+  <x-mail::button :url="$url">
+    @lang('setting::mail.btn.go_to_site')
+  </x-mail::button>
 </x-mail::message>
